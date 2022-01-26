dc764c7ab82b7c23b8c7313bd1f70884f630555d
stevehizmi <36041377+stevehizmi@users.noreply.github.com>
2022-01-25 18:55:14 -0600

    Add files via upload

---
+++ get_wifi_info.py
@@ -0,0 +1,37 @@
+import re
+import subprocess
+from subprocess import PIPE
+
+command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], stdout=PIPE, stderr=PIPE).stdout.decode()
+
+
+profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
+
+wifi_list = list()
+
+if len(profile_names) != 0:
+    for name in profile_names:
+
+        wifi_profile = dict()
+
+        profile_info = subprocess.run(["netsh", "wlan", "show", "profiles", name], stdout=PIPE, stderr=PIPE).stdout.decode()
+
+        if re.search("Security key           : Absent", profile_info):
+            continue
+
+        else:
+            wifi_profile["ssid"] = name
+
+            profile_info_pass = subprocess.run(["netsh", "wlan", "show", "profile", name, "key=clear"], stdout=PIPE, stderr=PIPE).stdout.decode()
+            password = re.search("Key Content            : (.*)\r", profile_info_pass)
+
+            if password == None:
+                wifi_profile["password"] = None
+
+            else:
+                wifi_profile["password"] = password[1]
+
+            wifi_list.append(wifi_profile)
+
+for x in range(len(wifi_list)):
+    print(wifi_list[x])
